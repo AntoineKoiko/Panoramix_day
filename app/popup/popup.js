f53583e49fa9ef25e0bf571dcdff56670d4b149d
AntoineKoiko <antoine.couacault@epitech.eu>
2025-12-03 22:16:49 +0100

    feat(popup.js & utils.js): move storage funciton in a utils file

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,27 +1,4 @@
-const storeValue = (key, value) => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.set({ [key]: value }, () => {
-            if (chrome.runtime.lastError) {
-                reject(new Error(chrome.runtime.lastError));
-            } else {
-                resolve();
-            }
-        });
-    });
-}
-
-const loadValue = (key) => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get(key, (result) => {
-            if (chrome.runtime.lastError) {
-                reject(new Error(chrome.runtime.lastError));
-            } else {
-                resolve(result[key]);
-            }
-        });
-    }
-    );
-}
+import { storeValue, loadValue } from "./utils.js";
 
 const authentifyAndGetEvents = async () => {
     try {
